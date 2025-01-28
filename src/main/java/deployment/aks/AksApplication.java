b9ba94d70923fb5870c1cd6857b4c3bb0813adf2
Ashutosh Dwivedi <ashutoshdwivediashu98@gmail.com>
2025-01-28 17:48:16 +0530

    New Changes in code

--- src/main/java/deployment/aks/AksApplication.java
+++ src/main/java/deployment/aks/AksApplication.java
@@ -3,15 +3,17 @@ package deployment.aks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AksApplication.class, args);
 
 	}
-	@GetMapping("/")
+	@GetMapping("/hey")
     public String hello() {
         return "Hello, Azure AKS!";
 
